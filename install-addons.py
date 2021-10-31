c22effa25a2b52858467cc3e0e0a56567cae144c
GandelXIV <filip@chovanec.com>
2021-10-31 19:40:43 +0100

    fixed indexing bug

--- install-addons.py
+++ install-addons.py
@@ -15,15 +15,17 @@ def rf(name):
 DEST = rf("destination.cfg").strip()
 
 def install(files, dest):
-    total_tasks = len(files)
-    for i, fn in enumerate(files):
+    total_tasks = len(list(filter(lambda x: iszip(x), files)))
+    i = 0
+    for fn in files:
         if iszip(fn):
-            print("[{0}/{1}] Copying:    {2}".format(i + 1, total_tasks, fn))
+            i += 1
+            print("[{0}/{1}] Copying:    {2}".format(i , total_tasks, fn))
             shutil.copy(fn, dest)
-            print("[{0}/{1}] Extracting: {2}".format(i + 1, total_tasks, fn))
+            print("[{0}/{1}] Extracting: {2}".format(i , total_tasks, fn))
             with zipfile.ZipFile(fn, 'r') as zip_ref:
                 zip_ref.extractall(dest) 
-            print("[{0}/{1}] Removing:   {2}".format(i + 1, total_tasks, fn))
+            print("[{0}/{1}] Removing:   {2}".format(i , total_tasks, fn))
             os.remove(dest + '/' + fn)
 
 def main():
