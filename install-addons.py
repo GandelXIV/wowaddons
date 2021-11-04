ccf611813819358bed91a9a8215598184abdba0b
Filip Chovanec <61948151+GandelXIV@users.noreply.github.com>
2021-11-04 21:11:18 +0100

    Update install-addons.py

--- install-addons.py
+++ install-addons.py
@@ -12,26 +12,31 @@ def rf(name):
     with open(name, 'r') as f:
         return f.read()
 
+def log(msg):
+    if LOGS:
+        print(msg)
+
 DEST = rf("destination.cfg").strip()
 ADDONS = "addons/"
+LOGS = True
 
 def install(files, dest):
     total_tasks = len(list(filter(lambda x: iszip(x), files)))
     i = 0
     for fn in files:
-        if iszip(fn):
-            i += 1
-            print("[{0}/{1}] Copying:    {2}".format(i , total_tasks, fn))
-            shutil.copy(ADDONS + fn, dest)
-            print("[{0}/{1}] Extracting: {2}".format(i , total_tasks, fn))
-            with zipfile.ZipFile(ADDONS + fn, 'r') as zip_ref:
-                zip_ref.extractall(dest) 
-            print("[{0}/{1}] Removing:   {2}".format(i , total_tasks, fn))
-            os.remove(dest + '/' + fn)
+        i += 1
+        log("[{0}/{1}] Copying:    {2}".format(i , total_tasks, fn))
+        shutil.copy(ADDONS + fn, dest)
+        log("[{0}/{1}] Extracting: {2}".format(i , total_tasks, fn))
+        with zipfile.ZipFile(ADDONS + fn, 'r') as zip_ref:
+            zip_ref.extractall(dest) 
+        log("[{0}/{1}] Removing:   {2}".format(i , total_tasks, fn))
+        os.remove(dest + '/' + fn)
 
 def main():
+    log("Starting...")
     install(os.listdir(ADDONS), DEST)
-    print("Done!")
+    log("Done!")
 
 if __name__ == "__main__":
     main()
